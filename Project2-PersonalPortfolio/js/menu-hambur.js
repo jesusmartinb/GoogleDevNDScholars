34fa8c73dd62cc54795591a52f5d4032f6ad362b
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2018-02-25 20:22:36 +0100

    Add project 2 Personal Portfolio

---
+++ Project2-PersonalPortfolio/js/menu-hambur.js
@@ -0,0 +1,16 @@
+/*
+ * Open the drawer when the menu ison is clicked.
+ */
+
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav-principal');
+
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
+
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
