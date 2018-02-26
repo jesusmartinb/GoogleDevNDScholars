96ede2ba5bde901bcbfba4374af92abdf6e087c3
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2018-02-26 10:39:19 +0100

    Some comments are entered in the files index.html styles.css and menu-hambur.js

--- Project2-PersonalPortfolio/js/menu-hambur.js
+++ Project2-PersonalPortfolio/js/menu-hambur.js
@@ -1,5 +1,11 @@
 /*
  * Open the drawer when the menu ison is clicked.
+
+ The open class is added with javascript when
+ you click on the hamburger menu,
+ scrolling the navigation on the canvas, when you
+ click on the canvas the open class disappears and
+ the navigation is retracted again
  */
 
 var menu = document.querySelector('#menu');
